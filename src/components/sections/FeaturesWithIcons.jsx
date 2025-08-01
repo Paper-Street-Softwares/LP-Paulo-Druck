f3b502ceb8415e30038fa279ea8d63c3924ee045
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-08-01 08:34:55 -0300

    fix: Altera estrutura da Features e insere copy

--- src/components/sections/FeaturesWithIcons.jsx
+++ src/components/sections/FeaturesWithIcons.jsx
@@ -22,7 +22,7 @@ export default function FeaturesWithIcons({ colorMode }) {
   const textClass = textClasses[colorMode] || textClasses.default;
 
   return (
-    <SectionArea id="service" className={`${bgClass} `} paddingbot={false}>
+    <SectionArea id="service" className={`${bgClass} `} paddingbot={true}>
       <SectionHeader
         className={`text-center ${textClass}`}
         miniTitle={content.texts.features.miniTag}
@@ -33,90 +33,52 @@ export default function FeaturesWithIcons({ colorMode }) {
         colorMode="dark"
       />
       <SectionWrapper>
-        {/* mobile */}
-        <div className="w-full flex flex-col items-center gap-2 pb-[48px] desktop1:gap-16 desktop1:hidden">
-          {/* Linha de cima: Azul e Verde */}
-          <div className="flex flex-col tablet1:flex-row tablet1:justify-center gap-5 w-full">
-            {/* Card Azul */}
-            <div className="w-full tablet1:w-[45%] desktop1:w-[28%] flex flex-col justify-center items-center">
-              <MotionDivDownToUp>
-                <IconFeatureCard
-                  icon={content.texts.features.card1.icon}
-                  title={content.texts.features.card1.title}
-                  paragraph={content.texts.features.card1.subtitle}
-                  className={` ${textClass}`}
-                  colorMode={colorMode}
-                />
-              </MotionDivDownToUp>
-            </div>
-
-            {/* Card Verde */}
-            <div className="w-full tablet1:w-[45%] desktop1:w-[28%] flex flex-col justify-center items-center  tablet1:mt-0">
-              <MotionDivDownToUp>
-                <IconFeatureCard
-                  icon={content.texts.features.card2.icon}
-                  title={content.texts.features.card2.title}
-                  paragraph={content.texts.features.card2.subtitle}
-                  className={textClass}
-                  colorMode={colorMode}
-                />
-              </MotionDivDownToUp>
-            </div>
-          </div>
-
-          {/* Card Vermelho - abaixo centralizado */}
-          <div className="w-full tablet1:w-[60%] desktop1:w-[35%] flex flex-col justify-center items-center">
-            <MotionDivDownToUp>
-              <IconFeatureCard
-                icon={content.texts.features.card3.icon}
-                title={content.texts.features.card3.title}
-                paragraph={content.texts.features.card3.subtitle}
-                className={` ${textClass}`}
-                colorMode={colorMode}
-              />
-            </MotionDivDownToUp>
-          </div>
-        </div>
-
-        {/* desktop */}
-        <div className="hidden desktop1:mt-[40px] flex-col items-center w-full tablet1:flex-row tablet1:justify-between desktop1:gap-x-0 desktop1:px-0 desktop1:flex">
-          <div className="col1 tablet1:w-[50%] desktop1:mb-[25%] desktop1:w-[28%] flex flex-col justify-center items-center">
+        <div className="flex desktop1:mt-[40px] flex-col items-center w-full tablet1:flex-row tablet1:justify-between desktop1:gap-x-0 desktop1:px-0">
+          <div className="col1 tablet1:w-[50%] desktop1:w-[28%] flex flex-col justify-center items-center">
             <MotionDivDownToUp>
               <IconFeatureCard
                 icon={content.texts.features.card1.icon}
                 title={content.texts.features.card1.title}
                 paragraph={content.texts.features.card1.subtitle}
-                className={` ${textClass}`}
+                className={`tablet1:mb-[16px] desktop1:mb-0 desktop2:mb-[46px] ${textClass}`}
+                colorMode={colorMode}
+              />
+            </MotionDivDownToUp>
+            <MotionDivDownToUp>
+              <IconFeatureCard
+                icon={content.texts.features.card2.icon}
+                title={content.texts.features.card2.title}
+                paragraph={content.texts.features.card2.subtitle}
+                className={textClass}
                 colorMode={colorMode}
               />
             </MotionDivDownToUp>
           </div>
 
-          <MotionDivDownToUp className="hidden desktop1:flex flex-col gap-12 justify-center w-[35%] h-[1200px]">
+          <MotionDivDownToUp className="hidden desktop1:flex justify-center w-[35%]">
             <div
               className="hidden h-[640px] w-full desktop1:flex col2 rounded-2xl bg-top bg-cover shadow-custom-opacity shadow-shadowFeatures/10"
               style={{
                 backgroundImage: `url(${content.texts.features.imgFeatures})`,
               }}
             ></div>
+          </MotionDivDownToUp>
 
-            <MotionDivDownToUp className="m-auto">
+          <div className="col3 tablet1:w-[50%] desktop1:w-[28%] flex flex-col justify-center items-center">
+            <MotionDivDownToUp>
               <IconFeatureCard
                 icon={content.texts.features.card3.icon}
                 title={content.texts.features.card3.title}
-                paragraph={content.texts.features.card3.description}
-                className={` ${textClass}`}
+                paragraph={content.texts.features.card3.subtitle}
+                className={`tablet1:mb-[16px] desktop1:mb-0 desktop2:mb-[46px] ${textClass}`}
                 colorMode={colorMode}
               />
             </MotionDivDownToUp>
-          </MotionDivDownToUp>
-
-          <div className="col3 tablet1:w-[50%] desktop1:w-[28%] desktop1:mb-[25%] flex flex-col justify-center items-center ">
             <MotionDivDownToUp>
               <IconFeatureCard
-                icon={content.texts.features.card2.icon}
-                title={content.texts.features.card2.title}
-                paragraph={content.texts.features.card2.subtitle}
+                icon={content.texts.features.card4.icon}
+                title={content.texts.features.card4.title}
+                paragraph={content.texts.features.card4.subtitle}
                 className={textClass}
                 colorMode={colorMode}
               />
